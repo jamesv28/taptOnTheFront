362806db9a4e4af484d400a6c158da1026c1c7c6
James Volmert <james.volmert@gmail.com>
2016-05-25 10:00:26 -0600

    created contact page

--- src/app/beerTypes/beerTypesServices.js
+++ src/app/beerTypes/beerTypesServices.js
@@ -6,7 +6,7 @@ angular
             getBeerTypes: function() {
                 return $http({
                     method: 'GET',
-                    url: 'https://tranquil-falls-61925.herokuapp.com/'
+                    url: 'http://tranquil-falls-61925.herokuapp.com/'
                 })
                     .then(function (data) {
                         console.log('this is data', data);
