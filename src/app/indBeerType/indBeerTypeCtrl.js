336dcd2121bca6ce1172d760c3316c780f3f356f
James Volmert <james.volmert@gmail.com>
2016-05-26 16:32:26 -0600

    finished basic template for individual beers

--- src/app/indBeerType/indBeerTypeCtrl.js
+++ src/app/indBeerType/indBeerTypeCtrl.js
@@ -1,7 +1,6 @@
 angular.module('app')
     .controller('indBeerTypesCtrl', ['$scope', '$stateParams', 'getIndBeersService', function ($scope, $stateParams, getIndBeersService) {
         // $scope.id = $routeParams.id;
-        console.log($stateParams.id);
         getIndBeersService.getBeer($stateParams.id)
             .then(function (data) {
                 $scope.beer = data.data;
