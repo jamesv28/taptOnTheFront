d4e6be17e28e825a7e0d7e75cd1188b56c55c6b2
James Volmert <james.volmert@gmail.com>
2016-05-25 21:17:49 -0600

    need to fix the google maps api documentation

--- src/app/pubCrawl/pubCrawlRoute.js
+++ src/app/pubCrawl/pubCrawlRoute.js
@@ -1,22 +1,11 @@
-// angular.module('app', ['uiGmapgoogle-maps','ui.router'])
-//     .config(['uiGmapGoogleMapApiProvider', function (uiGmapGoogleMapApiProvider,$stateProvider) {
-//         // uiGmapGoogleMapApiProvider.configure({
-//         //     key: 'AIzaSyDH1ga-OhA707S_v92ubO0DN66w0800Jro',
-//         //     libraries: 'weather,geometry,visualization'
-//         // });
-//
-//         $stateProvider
-//             .state('pubCrawl', {
-//                 url: '/pubCrawl',
-//                 templateUrl: 'app/pubCrawl/pubCrawl-template.html',
-//                 controller: 'pubCrawlCtrl'
-//             });
-//
-//     }]);
-// // angular.module('app', ['uiGmapgoogle-maps']).config(
-// //     ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
-// //         GoogleMapApiProviders.configure({
-// //             china: true
-// //         });
-// //     }]
-// // );
+angular.module('app')
+    .config( function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('pubCrawl', {
+                url: '/pubCrawl',
+                templateUrl: 'app/pubCrawl/pubCrawl-template.html'
+                // controller: 'pubCrawlCtrl'
+            });
+
+    });
