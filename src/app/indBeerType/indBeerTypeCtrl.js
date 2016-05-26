98ba69d6f39b9869b494f4c52c6897c0d14cab3d
James Volmert <james.volmert@gmail.com>
2016-05-26 13:10:03 -0600

    fixed ind route

--- src/app/indBeerType/indBeerTypeCtrl.js
+++ src/app/indBeerType/indBeerTypeCtrl.js
@@ -1,9 +1,10 @@
 angular.module('app')
-    .controller('indBeerTypesCtrl', ['$scope', '$routeParams', 'getIndBeersService', function ($scope,$routeParams, getIndBeersService) {
-        $scope.id = $routeParams.id;
-        console.log('hello');
-        getIndBeersService.getBeer($scope.id)
+    .controller('indBeerTypesCtrl', ['$scope', '$stateParams', 'getIndBeersService', function ($scope, $stateParams, getIndBeersService) {
+        // $scope.id = $routeParams.id;
+        console.log($stateParams.id);
+        getIndBeersService.getBeer($stateParams.id)
             .then(function (data) {
-                $scope.beer = data;
+                $scope.beer = data.data;
+                console.log(data);
             })
     }]); // end of controller
